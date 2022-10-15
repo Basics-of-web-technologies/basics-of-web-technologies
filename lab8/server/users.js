88d07f081ecfbba6bc4153b343e5e6aa5d8ef65e
RV8V <rv.volovik88@gmail.com>
2022-10-15 16:49:00 +0300

    feat(lab8): frontend, backend, docs

---
+++ lab8/server/users.js
@@ -0,0 +1,16 @@
+const users = [
+  {
+    email: 'admin@gmail.com',
+    password: 'password',
+    admin: true,
+    data: {
+      name: 'Ruslan Volovyk',
+      group: 'IP-93',
+      phone: '123456789',
+      card: 'AA №123456',
+      faculty: 'FICT',
+    },
+  },
+];
+
+module.exports = users;
